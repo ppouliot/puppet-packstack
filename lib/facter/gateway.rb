e84031c5d202cadb958fba86e8f56ba50b4089c6
Peter Pouliot <ppouliot@gmail.com>
2013-08-07 23:37:17 -0400

    files

--- lib/facter/gateway.rb
+++ lib/facter/gateway.rb
@@ -1,5 +1,5 @@
 Facter.add("gateway") do
   setcode do
-  facter::Util::Resolution.exec( netstat -nr |grep eth0 |grep UG |awk '{ print 2}')
+  facter::Util::Resolution.exec( `netstat -nr |grep eth0 |grep UG |awk '{ print 2}'`)
   end
 end
