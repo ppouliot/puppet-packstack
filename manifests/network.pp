587100051c1ab051c1191b8dec695a6bc1573573
Peter Pouliot <ppouliot@gmail.com>
2013-10-03 11:42:43 -0400

    needed to deal w/ multiple nic name issues

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,13 +1,23 @@
 class packstack::network {
 
-  file {'/etc/sysconfig/network-scripts/ifcfg-em1':
+  if $interface_type == 'eth' {
+    $iface1 = 'eth0', 
+    $iface2 = 'eth1', 
+  }
+
+  if $interface_type == 'em' {
+    $iface1 = 'em1', 
+    $iface2 = 'em2', 
+  }
+
+  file {'/etc/sysconfig/network-scripts/ifcfg-${iface1}":
     ensure => present,
-    content => template('packstack/ifcfg-em1.erb'),
+    content => template('packstack/ifcfg-eth0.erb'),
     notify => service['network'],
   }
-  file {'/etc/sysconfig/network-scripts/ifcfg-em2':
+  file {'/etc/sysconfig/network-scripts/ifcfg-${iface2}":
     ensure => present,
-    source => "puppet:///modules/packstack/ifcfg-em2",
+    content => template('packstack/ifcfg-eth1.erb'),
     notify => service['network'],
   }
 
@@ -21,6 +31,7 @@ class packstack::network {
     ensure => present,
     notify => service['network'],
     content => ["
+domain openstack.tld
 search openstack.tld
 nameserver 10.21.7.1
 nameserver 10.21.7.2
@@ -33,11 +44,11 @@ nameserver 10.21.7.2
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    subscribe => File['/etc/sysconfig/network-scripts/ifcfg-em1',
-                      '/etc/sysconfig/network-scripts/ifcfg-em2',
+    subscribe => File["/etc/sysconfig/network-scripts/ifcfg-${iface1}",
+                      "/etc/sysconfig/network-scripts/ifcfg-${iface2}",
                       '/etc/sysconfig/network','/etc/resolv.conf'],
   }
-File['/etc/sysconfig/network-scripts/ifcfg-em1'] ->
-  File['/etc/sysconfig/network-scripts/ifcfg-em2'] ->
+File['/etc/sysconfig/network-scripts/ifcfg-eth0'] ->
+  File['/etc/sysconfig/network-scripts/ifcfg-eth1'] ->
     File['/etc/sysconfig/network']->Service['network']
 }
