83b428d7b74b881428fd7bed9fa01bf952214f88
Peter Pouliot <ppouliot@gmail.com>
2013-10-03 10:06:13 -0400

    fixing issues w/ network card naming

--- lib/facter/default_gateway.rb
+++ lib/facter/default_gateway.rb
@@ -1,6 +1,6 @@
 require 'facter'
 Facter.add("default_gateway") do
   setcode do
-  Facter::Util::Resolution.exec( "netstat -nr |grep eth0 |grep UG |awk '{ print $2}'" )
+  Facter::Util::Resolution.exec( "netstat -nr |grep UG |awk '{ print $2}'" )
   end
 end
--- manifests/network.pp
+++ manifests/network.pp
@@ -1,13 +1,13 @@
 class packstack::network {
 
-  file {'/etc/sysconfig/network-scripts/ifcfg-eth0':
+  file {'/etc/sysconfig/network-scripts/ifcfg-em1':
     ensure => present,
-    content => template('packstack/ifcfg-eth0.erb'),
+    content => template('packstack/ifcfg-em1.erb'),
     notify => service['network'],
   }
-  file {'/etc/sysconfig/network-scripts/ifcfg-eth1':
+  file {'/etc/sysconfig/network-scripts/ifcfg-em2':
     ensure => present,
-    source => "puppet:///modules/packstack/ifcfg-eth1",
+    source => "puppet:///modules/packstack/ifcfg-em2",
     notify => service['network'],
   }
 
@@ -33,11 +33,11 @@ nameserver 10.21.7.2
     enable => true,
     hasstatus => true,
     hasrestart => true,
-    subscribe => File['/etc/sysconfig/network-scripts/ifcfg-eth0',
-                      '/etc/sysconfig/network-scripts/ifcfg-eth1',
+    subscribe => File['/etc/sysconfig/network-scripts/ifcfg-em1',
+                      '/etc/sysconfig/network-scripts/ifcfg-em2',
                       '/etc/sysconfig/network','/etc/resolv.conf'],
   }
-File['/etc/sysconfig/network-scripts/ifcfg-eth0'] ->
-  File['/etc/sysconfig/network-scripts/ifcfg-eth1'] ->
+File['/etc/sysconfig/network-scripts/ifcfg-em1'] ->
+  File['/etc/sysconfig/network-scripts/ifcfg-em2'] ->
     File['/etc/sysconfig/network']->Service['network']
 }
