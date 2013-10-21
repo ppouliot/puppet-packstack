59d37b028fe2cfdf07cfd72ed6b3f139954f96bf
Peter Pouliot <ppouliot@gmail.com>
2013-10-21 11:19:41 -0400

    typo in vlan range

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,6 +6,6 @@ class packstack::params {
   $packstack_packages  = [ 'openstack-packstack','openstack-utils' ]
   $rdo_release_rpm_url = "http://rdo.fedorapeople.org/openstack/openstack-${openstack_release}/rdo-release-${openstack_release}.rpm"
   $ntp_server_pool     = ['0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org']
-  $vlan_range          = "500-1000"
+  $vlan_range          = "500:1000"
   $cinder_volume_size  = "5G"
 }
