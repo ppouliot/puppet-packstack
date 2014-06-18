ec8c23b326918ba7702bbb1e8f765b3e2ed6481e
primeministerp <primeministerpete@gmail.com>
2014-06-18 13:47:10 -0400

    updating repo to match production

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class packstack::params {
   $sshkey_path          = "/root"
   $ssh_pubkey           = "/root/.ssh/id_dsa.pub"
   $answerfile           = "/root/packstack_answers.txt"
-  $openstack_release    = undef
+  $openstack_release    = 'havana'
   $packstack_packages   = [ 'openstack-packstack','openstack-utils' ]
   $rdo_release_rpm_url  = "http://rdo.fedorapeople.org/openstack/openstack-${openstack_release}/rdo-release-${openstack_release}.rpm"
   $ntp_server_pool      = ['0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org']
