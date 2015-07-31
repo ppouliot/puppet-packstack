ebc7c3ef53d6be15a07c8ba4900734c80b79e478
primeministerp <primeministerpete@gmail.com>
2015-07-31 03:23:03 -0400

    Fixed rdo release and url in packstack module

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,8 @@ class packstack::params {
   $answerfile           = "/root/packstack_answers.txt"
   $openstack_release    = 'havana'
   $packstack_packages   = [ 'openstack-packstack','openstack-utils' ]
-  $rdo_release_rpm_url  = "http://rdo.fedorapeople.org/openstack/openstack-${openstack_release}/rdo-release-${openstack_release}.rpm"
+# $rdo_release_rpm_url  = "http://rdo.fedorapeople.org/openstack/EOL/openstack-${openstack_release}/rdo-release-${openstack_release}.rpm"
+  $rdo_release_rpm_url  = "http://rdo.fedorapeople.org/openstack/EOL/openstack-${openstack_release}/rdo-release-9.noarch.rpm"
   $ntp_server_pool      = ['0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org']
   $vlan_range           = "500:1000"
   $cinder_volume_size   = "5G"
